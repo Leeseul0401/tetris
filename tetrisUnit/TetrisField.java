325c561455a30f8507a825c302770a9d4e603e98
Leeseul0401 <7836764@naver.com>
2023-03-07 00:42:53 +0900

    game field 생성

---
+++ tetrisUnit/TetrisField.java
@@ -0,0 +1,25 @@
+package tetrisUnit;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+public class TetrisField extends JPanel {
+	
+
+	public TetrisField() {
+		init();
+		setDisplay();
+	}
+	
+	private void init() {
+		
+	}
+	
+	private void setDisplay() {
+		
+	}
+
+}
