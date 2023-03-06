a5763d44d156716fc4df27f6813b3e91e830dae7
Leeseul0401 <7836764@naver.com>
2023-03-07 00:50:38 +0900

    game field 생성

--- tetrisUnit/TetrisField.java
+++ tetrisUnit/TetrisField.java
@@ -7,19 +7,18 @@ import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
 public class TetrisField extends JPanel {
-	
 
 	public TetrisField() {
 		init();
 		setDisplay();
 	}
-	
+
 	private void init() {
-		
+
 	}
-	
+
 	private void setDisplay() {
-		
+
 	}
 
 }
--- tetrisUnit/TetrisLayout.java
+++ tetrisUnit/TetrisLayout.java
@@ -10,49 +10,49 @@ import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
 public class TetrisLayout extends JFrame {
-	
+
 	private TetrisField gameField;
-	
+
 	private JPanel base;
 	private JLabel lblNorth;
-	
+
 	public TetrisLayout() {
 		showFrame();
 		init();
 		setDisplay();
 	}
-	
+
 	private void init() {
 		Font font = new Font("Fixedsys 보통", Font.BOLD, 40);
 		lblNorth = new JLabel("Tetris", JLabel.CENTER);
 		// background 할때만 setOpaque 씀?
-		//lblNorth.setOpaque(true);
+		// lblNorth.setOpaque(true);
 		lblNorth.setForeground(Color.WHITE);
 		lblNorth.setFont(font);
-		
+
 		JPanel pnlWest = new JPanel();
 		pnlWest.setBackground(Color.BLACK);
 		pnlWest.setBorder(new LineBorder(Color.WHITE, 3));
-		
-//		JPanel pnlMain = new JPanel(new BorderLayout());
-//		pnlMain.add(pnlWest, BorderLayout.WEST);
-//		pnlMain.setBackground(Color.BLACK);
-//		
-//		add(pnlMain, BorderLayout.CENTER);
-		
+
+		// JPanel pnlMain = new JPanel(new BorderLayout());
+		// pnlMain.add(pnlWest, BorderLayout.WEST);
+		// pnlMain.setBackground(Color.BLACK);
+		//
+		// add(pnlMain, BorderLayout.CENTER);
+
 	}
-	
+
 	private void setDisplay() {
 		JPanel pnlNorth = new JPanel();
 		pnlNorth.add(lblNorth);
 		pnlNorth.setBackground(Color.BLACK);
-		
+
 		// gameField = new TetrisField();
 		JPanel pnlWest = new JPanel();
 		pnlWest.setBackground(Color.BLACK);
 		pnlWest.setBorder(new LineBorder(Color.WHITE, 3));
 		pnlWest.setSize(400, 300);
-		
+
 		setVisible(true);
 		add(pnlNorth, BorderLayout.NORTH);
 		add(pnlWest, BorderLayout.WEST);
@@ -62,12 +62,12 @@ public class TetrisLayout extends JFrame {
 		setTitle("tetris");
 		setSize(550, 750);
 		setLocation(500, 50);
-//		setLocationRelativeTo(null);
+		// setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 		setResizable(false);
 	}
-	
+
 	public static void main(String[] args) {
 		new TetrisLayout();
 	}
